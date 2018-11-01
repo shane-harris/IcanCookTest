a3fed2caed81c47b558e19739ef9784435df6c39
rockb <rockb@10.0.0.113>
2018-10-31 23:00:55 -0700

    First Commit

---
+++ ICanCook/src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+	/*This will be where we can brain storm the application
+	 * I just wanted to get something started so nothing
+	 * here is considered permanent.
+	 * */
+}
